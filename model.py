ec396ea76e30f61d6869bd6b5971c71322ded3ad
Abhishek Pal <dev.04.palabhishek@gmail.com>
2026-09-04 18:50:51 +0530

    Step 2: alphas_from_betas

--- model.py
+++ model.py
@@ -20,8 +20,14 @@ def linear_beta_schedule(T: int, beta_start: float = 1e-4, beta_end: float = 0.0
     # TODO: return a linear beta schedule of length T
     pass
 
-# Step 2 - alphas_from_betas (not yet solved)
-# TODO: implement
+# Step 2 - alphas_from_betas
+import torch
+import torch.nn.functional as F
+
+def alphas_from_betas(betas):
+    # TODO: return 1 - betas
+    return 1.0- betas
+    pass
 
 # Step 3 - cumprod_alphas (not yet solved)
 # TODO: implement
